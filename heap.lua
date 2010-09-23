0a0b3eb99a969ddec73b51a1a95f898da05546b1
cubicdaiya <cubicdaiya@gmail.com>
2010-09-23 11:20:41 +0900

    add self

--- heap.lua
+++ heap.lua
@@ -20,6 +20,37 @@ function Heap:new (arr)
    function self.right(self, i)
       return 2 * i + 1
    end
+   
+   function maximum (self)
+      return self.a[1]
+   end
+
+   function self.extract_max (self)
+      if self.n < 1 then
+         error("heap underflow")
+      end
+      max       = self.a[1]
+      self.a[1] = self.a[self.n]
+      self.n    = self.n - 1
+      self.max_heapify(self.a, 1)
+      return max
+   end
+
+   function self.increase_key (self, i, key)
+      if key < self.a[i] then
+         error("new key is lesser than current key")
+      end
+      
+      self.a[i] = key
+      while i > 1 and a[self.parent(i)] < self.a[i] do
+         self.a[i], self.a[self.parent(i)] = self.a[self.parent(i)], self.a[i]
+         i = self.parent(i)
+      end
+   end
+
+   function self.insert (self, key)
+      self.increase_key(self, self.n, key)
+   end
 
    function self.max_heapify (self, a, i)
       l = self.left(self, i)
@@ -41,19 +72,19 @@ function Heap:new (arr)
       end
    end
    
-   function self.build_max_heap (self, a)
-      for i=#a / 2, 1, -1 do
-         self.max_heapify(self, a, i)
+   function self.build_max_heap (self)
+      for i=#self.a / 2, 1, -1 do
+         self.max_heapify(self, self.a, i)
       end
    end
 
    function self.sort (self)
-      heap:build_max_heap(self.a)
+      self.build_max_heap(self)
       orig = self.n
       for i=#a, 2, -1  do
          self.a[1], self.a[i] = self.a[i], self.a[1]
          self.n = self.n - 1
-         heap:max_heapify(self.a, 1);
+         self.max_heapify(self, self.a, 1);
       end
       self.n = orig
    end
